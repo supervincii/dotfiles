1df6a2fe19ef36527f6eedbe30bea41d1f35ad16
supervincii <74002397+supervincii@users.noreply.github.com>
2025-04-07 19:34:41 +0800

    add early-init file for LSP performance

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,8 @@
+;;; early-init --- code before initialization -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(setenv "LSP_USE_PLISTS" "true")
+
+(provide 'early-init)
+;;; early-init.el ends here
